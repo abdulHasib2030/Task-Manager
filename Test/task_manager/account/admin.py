36d0d7c2ebdd20909df03ac496c3d4d330e345ad
Abdul Hasib <136143027+abdulHasib2030@users.noreply.github.com>
2023-10-25 16:53:34 +0600

    Delete Test directory

--- Test/task_manager/account/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from account.models import *
-
-# Register your models here.
-class UserAccountAdmin(admin.ModelAdmin):
-  list_display = [ 'id', 'birth_date', 'gender']
-admin.site.register(UserAccount, UserAccountAdmin)
